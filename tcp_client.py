afaa91b7bf14ecf68d60878a2669550eb58b2f2c
Adam Borocz <boroczadam@gmail.com>
2009-10-25 18:38:25 +0000

    Just for the hell of it.

--- tcp_client.py
+++ tcp_client.py
@@ -82,14 +82,6 @@ work_list = [
 
 for work in work_list:
 	
-	#cmd = ''
-	#cmd = 'mkd|/tst/dir'
-	#cmd = 'upl|/tst/dir/file.jpg|dir/file.jpg'
-	#cmd = 'del|dir/file.jpg'
-	#cmd = 'inf'
-	#cmd = 'invalid'
-	#cmd = 'get|3'
-	
 	s.send(base64.b64encode(work)+"\n")
 	v = base64.b64decode(s.recv(1024))
 	print v
