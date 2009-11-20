fdd530950906296be02321f8273ae80ad8c50006
Adam Borocz <boroczadam@gmail.com>
2009-11-20 18:00:25 +0000

    Feeder now generates the correct instructions for the Q.

---
+++ feed_test.py
@@ -0,0 +1,4 @@
+import s3_feeder
+
+feeder = s3_feeder.Feeder("/Users/adamb/Documents/Activity reports","",2,0)
+feeder.run()
--- s3_feeder.py
+++ s3_feeder.py
@@ -3,43 +3,73 @@
 # Also has option to only upload missing items
 
 import os, socket, base64
+import s3_config
 
 class Feeder:
 	
+	# The starting point for feeding files
 	feed_root = ""
+	
+	# The key prefix we'll use when uploading into S3 (without trailing '/')
+	key_root = ""
+	
+	# The maximum depth for recursion
+	#	1 = current folder only
+	#	0 = Unlimited
+	#	> 1 = Go to specified depth
 	max_depth = 1
+	
+	# Whether to Query S3 for a list of files already in place
 	check_missing = 0
 	
-	def __init__ (self, feed_root, max_depth=1, check_missing=0):
+	def __init__ (self, feed_root, key_root, max_depth=1, check_missing=0):
 		self.feed_root = feed_root
+		self.key_root = key_root
 		self.max_depth = max_depth
 		self.check_missing = check_missing
 	
 	def run(self):
-		self.list_and_feed(self.feed_root, 1)
+		self.list_and_feed(self.feed_root, "", 1)
 	
 	
-	def list_and_feed(self, folder, current_depth):
+	def list_and_feed(self, folder, relative_path, current_depth):
 		
-		item_list = [os.path.join(folder,x) for x in os.listdir(folder)]
+		#item_list = [os.path.join(folder,x) for x in os.listdir(folder)]
 		
 		#If check missing flag is set, get Amazon Listing Here
 			
 		folders = []
-		for item in item_list:
+		#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#try:
+		#	s.connect((host, port))
+		#except: raise Exception("Failed to connect to Queue server")
+		
+		for item in os.listdir(folder):
 			
-			if os.path.isdir(item):
-				print ((current_depth - 1) * "\t")+"Adding Folder to Q: "+str(item)
-				folders.append(item)
+			item_with_path = os.path.join(folder,item)
+			
+			if os.path.isdir(item_with_path):
+				work = "mkd|%s" % os.path.join(self.key_root, relative_path, item)
+				#s.send(base64.b64encode(work)+"\n")
+				#v = base64.b64decode(s.recv(1024))
 				
-			elif os.path.isfile(item):
-				print ((current_depth - 1) * "\t")+"Adding file to Q: "+str(item)
+				folders.append(item_with_path)
+				
+			elif os.path.isfile(item_with_path):
+				work = "upl|%s|%s" % (item_with_path, os.path.join(self.key_root, relative_path, item))
+				#s.send(base64.b64encode(work)+"\n")
+				#v = base64.b64decode(s.recv(1024))
+
 			else:
 				print "UNKNOWN item: "+str(item)
+			
+			print work
+		
+		#s.close()
 		
 		
-		if current_depth < self.max_depth:
+		if self.max_depth == 0 or current_depth < self.max_depth:
 			for folder in folders:
-				self.list_and_feed(folder, current_depth+1)
+				self.list_and_feed(folder, os.path.join(relative_path,os.path.split(folder)[1]), current_depth+1)
 		
 		return 1
