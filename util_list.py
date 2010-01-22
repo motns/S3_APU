34a220bcc675697bcedee4bf66a20698a812c00d
Adam Borocz <adamb@couturelab.com>
2010-01-22 11:56:12 +0000

    List utility can now take the name of an alternative bucket to list form as the 3rd argument.

--- util_list.py
+++ util_list.py
@@ -5,7 +5,7 @@
 
 import s3_config
 import s3_output
-import os,sys
+import os, re, sys
 
 try:
 	prefix = str(sys.argv[1])
@@ -16,6 +16,14 @@ try:
 	limit = int(sys.argv[2])
 except: limit = 0
 	
+try:
+	bucket = str(sys.argv[3]).strip()
+	if re.match('[a-zA-Z0-9\-\_\.]+$', bucket) == None:
+		print "Invalid bucket name! Falling back to default."
+		raise Exception("Invalid")
+except:
+	bucket = s3_config.s3_bucket
+	
 #Little hack for convenience
 # Will use the first argument as 'limit', if it doesn't look like a path
 if prefix != "" and prefix[-1] != "/":
@@ -24,5 +32,7 @@ if prefix != "" and prefix[-1] != "/":
 		prefix = ""
 	except: pass
 	
-for k in s3_output.list_objects(s3_config.s3_bucket, os.path.join(prefix, ''), "", limit, "/"):
-	print k
+out = s3_output.list_objects(bucket, os.path.join(prefix, ''), "", limit, "/")
+if out != None and out != 0:
+	for k in out:
+		print k
